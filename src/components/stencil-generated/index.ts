f1074f33592e56f3b08a0d7c6f03a009658f3c9b
Miglė Ankštutytė <migle.ankstutyte@gmail.com>
2022-08-30 12:26:30 +0300

    First commit

--- src/components/stencil-generated/index.ts
+++ src/components/stencil-generated/index.ts
@@ -1,30 +1,12 @@
-/* 
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
 
-auto-generated react proxies will replace this file 
-if you use "../your-react-library-name/src/components/stencil-generated/index.ts" for your proxies target:
+import type { JSX } from 'carousel';
 
-*/
+import { defineCustomElements } from 'carousel/loader';
 
-/* SAMPLE: 
-import { reactOutputTarget as react } from '@stencil/react-output-target'
-
-export const config: Config = {
-  outputTargets: [
-    react({
-      componentCorePackage: 'your-stencil-library-name',
-      proxiesFile: '../your-react-library-name/src/components/stencil-generated/index.ts',
-      includeDefineCustomElements: true,
-    });
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-      dir: "./dist/custom-elements"
-    },
-  ],
-}
-*/
-
-export const object = {};
+defineCustomElements();
+export const MyCarousel = /*@__PURE__*/createReactComponent<JSX.MyCarousel, HTMLMyCarouselElement>('my-carousel');
+export const MyNavigator = /*@__PURE__*/createReactComponent<JSX.MyNavigator, HTMLMyNavigatorElement>('my-navigator');
